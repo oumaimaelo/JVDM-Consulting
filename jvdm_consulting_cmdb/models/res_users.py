cbb2c04e293e3639bbe9007c28db2518952ceee5
hananebarrah <hananbarah@gmail.com>
2020-06-26 11:19:55 +0100

    -> Add the res_users.py file: Create a model for CMDB users.

--- jvdm_consulting_cmdb/models/res_users.py
+++ jvdm_consulting_cmdb/models/res_users.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
-#
-#
+from odoo import models, fields, api
+
+
 # class Res_Users(models.Model):
 #     _inherit = 'res.users'
 #
 #     landscp_read_access = fields.Boolean(string="Read Access", default=True)
 #     landscp_write_access = fields.Boolean(string="Write Access", default=True)
+
+class CMDBUsers(models.Model):
+    _name = 'cmdb.users'
+
+    user_id = fields.Many2one(comodel_name="res.users", string="User")
+    landscp_read_access = fields.Boolean(string="Read Access", default=False)
+    landscp_write_access = fields.Boolean(string="Write Access", default=False)
